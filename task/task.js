ce6b8ab98ab6ed49bc1b28a9371daec5197beff7
alaac4q <alaaalsudani@ac.c4q.nyc>
2017-10-09 00:29:46 -0400

    new task.js

--- task/task.js
+++ task/task.js
@@ -2,13 +2,14 @@
 
 var readline = require('readline')
 
-// We will be able to use `rl`
-// Only After creating the readline interface
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
+function clear () {
+  process.stdout.write('\u001B[2J\u001B[0;0f')
+}
 
 function createTask(title, completed){
     var task = {
@@ -17,19 +18,46 @@ function createTask(title, completed){
     }
     return task
 }
-rl.on('line', function(input) {
-    
-var tasks = input.split(' ')
-if (tasks[0] === "add"){
-tasks.push(createTask(tasks[1]+' '+tasks[2]  , false))
 
-console.log(tasks[3])
-}
-else{
-    console.log("the first word must be add")
-}
+taskArr = []
 
+function toggle(arr , id){
+    if(arr[id].completed === false ){
+        arr[id].completed = true
+        console.log(arr[id].title.slice(3) + " is completed")
+    } else if (arr[id].completed === true){
+        arr[id].completed = false
+        console.log(arr[id].title.slice(3) + " is not completed ")
+    }
+    }
+
+rl.on('line', function (input) {
+  var updateInput = input.toLowerCase().split(' ')
+   if (updateInput[0] === 'add')
+   {   
+    taskArr.push(createTask(input,false))
+        for (var i = 0 ; i < taskArr.length ;i++){
+           console.log(i + " " +taskArr[i].title.slice(3) + ". " + ' completed ; ' + taskArr[i].completed )} 
+    } else if(updateInput[0] === 'toggle' & updateInput[1] !== undefined){
+           var id = Number(updateInput[1])
+           toggle(taskArr , id)
+    } else if (updateInput[0] === 'show' & updateInput[1] === 'all'){
+        for (var i = 0 ; i < taskArr.length ;i++){
+           console.log(i + " " +taskArr[i].title.slice(3) + ". " + ' completed ; ' + taskArr[i].completed ) }
+    } else if (updateInput[0] === 'show' & updateInput[1] === 'active' ){
+        for (var i = 0 ; i < taskArr.length ;i++){
+            if(taskArr[i].completed === false){
+            console.log(i + " " +taskArr[i].title.slice(3) + ". " + ' completed ; ' + taskArr[i].completed ) }
+        }
+    } else if (updateInput[0] === 'show' & updateInput[1] === 'completed' ){
+        for (var i = 0 ; i < taskArr.length ;i++){
+            if(taskArr[i].completed === true){
+            console.log(i + " " +taskArr[i].title.slice(3) + ". " + ' completed ; ' + taskArr[i].completed ) }
+        }
+    }
+    console.log('choose and type one of the option { ADD , toggle , show Active , show Completed }')
 })
-
-
-
+    
+    if (taskArr = [] ){
+        console.log("the list is empty you need to add task first")
+    }
