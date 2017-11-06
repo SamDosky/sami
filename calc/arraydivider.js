045a48b9b1948e9e21f3f867975946168453ab7e
alaac4q <alaaalsudani@ac.c4q.nyc>
2017-11-06 12:18:23 -0500

    calculater

---
+++ calc/arraydivider.js
@@ -0,0 +1,20 @@
+
+// var str ="1000+55+3"
+
+// // console.log((parseInt(str).length)) 
+  
+//   var c = (parseInt(str)).toString().length
+// //   c = d.toString().length
+// var d = (parseInt(str.slice(c+1))).toString().length + c
+
+
+//   if((str.slice(c , c+1)) === "+"){
+
+//     var g = parseInt(str.slice(0)) + parseInt(str.slice(c+1)) + parseInt(str.slice(d+1))
+//   }
+
+// console.log(d)
+//   console.log(g)
+
+var c = eval ("4+5")
+console.log(c)
